f56854dd3b124a1210ac59e0bcd0443e9b92b6c9
Latrell Lee <latrell@Latrells-MacBook-Air.local>
2021-05-22 14:17:10 -0400

    refactored home page

--- routes/home.js
+++ routes/home.js
@@ -1,34 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const awsConfig = require('aws-config');
-const AWS = require('aws-sdk');
+const homeController = require('../controllers/home');
 
-router.get("/", (req, res, next) => {
-  // testing AWS S3 get object
-  const file = 'Headshot.JPG';
-  const s3 = new AWS.S3({});
-  let base64;
-  const params = {
-    Bucket: 'mywebsitepictures',
-    Key: file
-  };
-  s3.getObject(params, function(err, data) {
-    if (err) {
-      console.log(err, err.stack);
-    }
-    else {
-      base64 = data.Body.toString('base64');
-      let src = "data:image/jpeg;base64,"+base64;
-      res.render("home", {
-        pageTitle: "Home",
-        heading: "Welcome!",
-        path: "/",
-        isAdmin: req.session.isLoggedIn,
-        imageURL: src
-      });
-    }
-  })
-});
+
+router.get("/", homeController.getSections);
+
+
+
 
 module.exports = {
   routes: router,
