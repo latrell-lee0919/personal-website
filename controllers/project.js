e57a1a550a99d5cd58a53a5ddee6a055b0b2820d
Latrell Lee <latrell@Latrells-MacBook-Air.local>
2021-05-23 22:23:02 -0400

    added functionality that renders projects based on their categories

--- controllers/project.js
+++ controllers/project.js
@@ -3,8 +3,26 @@ const Project = require("../models/project");
 exports.getProjectPreview = (req, res) => {
   Project.find()
     .then((projects) => {
+      //console.log(projects);
+      let webDev = [];
+      let mobileDev = [];
+      let machineLearning = [];
+      projects.forEach(function (project) {
+        let category = project.category;
+        if (category == "Web Development") {
+          webDev.push(project)
+        } else if (category == "Mobile App Development") {
+          mobileDev.push(project)
+        } else if (category == "Machine Learning") {
+          machineLearning.push(project)
+        }
+        console.log(webDev);
+      })
       res.render("project/project-preview", {
         projects: projects,
+        webDevelopment: webDev,
+        mobileDevelopment: mobileDev,
+        machineLearning: machineLearning,
         pageTitle: "Projects",
         path: "/projects",
         isAdmin: req.session.isLoggedIn,
