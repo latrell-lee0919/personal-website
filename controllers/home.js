e0615de8e5efc02588380785b950d9c438c8a538
Latrell Lee <latrell@Latrells-MacBook-Air.local>
2021-05-29 23:59:19 -0400

    testing consumption of ig api

--- controllers/home.js
+++ controllers/home.js
@@ -1,32 +1,40 @@
-const awsConfig = require('aws-config');
 const AWS = require('aws-sdk');
-const home = require('../routes/home');
+const Instagram = require('instagram-web-api');
+const User = require('../models/user');
+require('dotenv').config();
+const username = process.env.username;
+const password = process.env.password;
 
 exports.getSections = (req, res, next) => {
-        // testing AWS S3 get object
-        const file = 'Headshot.JPG';
-        const s3 = new AWS.S3({});
-        let base64;
-        const params = {
-          Bucket: 'mywebsitepictures',
-          Key: file
-        };
-        s3.getObject(params, function(err, data) {
-          if (err) {
-            console.log(err, err.stack);
-          }
-          else {
-            base64 = data.Body.toString('base64');
-            let src = "data:image/jpeg;base64,"+base64;
-            res.render("home/home", {
-              pageTitle: "Home",
-              heading: "Welcome!",
-              path: "/",
-              isAdmin: req.session.isLoggedIn,
-              imageURL: src
-            });
-          }
-        })
+    const client = new Instagram({ username, password })
+    client
+      .login()
+      .then(() => {
+        client
+          .getProfile()
+          .then(console.log(client.getProfile()))
+      })
+    const file = 'Headshot.JPG';
+    const s3 = new AWS.S3({});
+    let base64;
+    const params = {
+      Bucket: 'mywebsitepictures',
+      Key: file
+    };
+    s3.getObject(params, function(err, data) {
+      if (err) {
+        console.log(err, err.stack);
       }
-
-    
+      else {
+        base64 = data.Body.toString('base64');
+        let src = "data:image/jpeg;base64,"+base64;
+        res.render("home/home", {
+          pageTitle: "Home",
+          heading: "Welcome!",
+          path: "/",
+          isAdmin: req.session.isLoggedIn,
+          imageURL: src
+        });
+      }
+    })
+  }
